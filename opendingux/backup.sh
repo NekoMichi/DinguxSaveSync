ba095d391204bac80b716ae2acae08a56e21b08c
NekoMichi <31323046+NekoMichi@users.noreply.github.com>
2020-07-10 13:14:58 +0100

    Minor cleanup, added path shortcuts

--- opendingux/backup.sh
+++ opendingux/backup.sh
@@ -3,9 +3,12 @@
 # desc=Backup save data to external SD card
 # author=NekoMichi
 
+INTPATH="/media/data/local/home/"
+EXTPATH="/media/sdcard/"
+
 echo "===Backup Saves==="
 # Checks to see if there is a card inserted in slot 2
-if [ ! -d /media/sdcard/ ]; then
+if [ ! -d $EXTPATH ]; then
 	echo "Could not detect secondary micro SD card."
 	echo "Please make sure you have a secondary SD card inserted."
 	echo ""
@@ -14,106 +17,106 @@ if [ ! -d /media/sdcard/ ]; then
 fi
 
 # Checks to see if backup folder exists on card 2, if not then will create it
-if [ ! -d /media/sdcard/backup/ ]; then
+if [ ! -d $EXTPATH/backup/ ]; then
 	echo "Backup folder doesn't exist, creating folder"
-	mkdir /media/sdcard/backup
+	mkdir $EXTPATH/backup
 fi
 
 # Backs up GMenu2x data
-if [ ! -d /media/sdcard/backup/gmenu2x/ ]; then
-	echo "Gmenu2x backup folder doesn't exist, creating folder"
-	mkdir /media/sdcard/backup/gmenu2x
-fi
-echo "Backing up Gmenu2x data"
-rsync --update -rt /media/data/local/home/.gmenu2x/ /media/sdcard/backup/gmenu2x
+# if [ ! -d $EXTPATH/backup/gmenu2x/ ]; then
+#	echo "Gmenu2x backup folder doesn't exist, creating folder"
+#	mkdir $EXTPATH/backup/gmenu2x
+# fi
+# echo "Backing up Gmenu2x data"
+# rsync --update -rt $INTPATH/.gmenu2x/ $EXTPATH/backup/gmenu2x
 
 # Backs up screenshots
-if [ -d /media/data/local/home/screenshots/ ]; then
-	if [ ! -d /media/sdcard/backup/screenshots/ ]; then
+if [ -d $INTPATH/screenshots/ ]; then
+	if [ ! -d $EXTPATH/backup/screenshots/ ]; then
 		echo "Screenshots backup folder doesn't exist, creating folder"
-		mkdir /media/sdcard/backup/screenshots
+		mkdir $EXTPATH/backup/screenshots
 	fi
 	echo "Backing up screenshots"
-	rsync --update -rt /media/data/local/home/screenshots/ /media/sdcard/backup/screenshots
+	rsync --update -rt $INTPATH/screenshots/ $EXTPATH/backup/screenshots
 fi
 
 # Backs up scripts
-if [ -d /media/data/local/home/.scriptrunner/ ]; then
-	if [ ! -d /media/sdcard/backup/scriptrunner/ ]; then
-		echo "Scripts backup folder doesn't exist, creating folder"
-		mkdir /media/sdcard/backup/scriptrunner
-	fi
-	echo "Backing up scripts"
-	rsync --update -rt /media/data/local/home/.scriptrunner/ /media/sdcard/backup/scriptrunner
-fi
+# if [ -d $INTPATH/.scriptrunner/ ]; then
+#	if [ ! -d $EXTPATH/backup/scriptrunner/ ]; then
+#		echo "Scripts backup folder doesn't exist, creating folder"
+#		mkdir $EXTPATH/backup/scriptrunner
+#	fi
+#	echo "Backing up scripts"
+#	rsync --update -rt $INTPATH/.scriptrunner/ $EXTPATH/backup/scriptrunner
+# fi
 
 # Backs up FCEUX data
-if [ -d /media/data/local/home/.fceux/ ]; then
-	if [ ! -d /media/sdcard/backup/fceux/ ]; then
+if [ -d $INTPATH/.fceux/ ]; then
+	if [ ! -d $EXTPATH/backup/fceux/ ]; then
 		echo "FCEUX backup folder doesn't exist, creating folder"
-		mkdir /media/sdcard/backup/fceux
+		mkdir $EXTPATH/backup/fceux
 	fi
 	echo "Backing up FCEUX data"
-	rsync --update -rt /media/data/local/home/.fceux/ /media/sdcard/backup/fceux
+	rsync --update -rt $INTPATH/.fceux/ $EXTPATH/backup/fceux
 fi
 
 # Backs up Gambatte data
-if [ -d /media/data/local/home/.gambatte/ ]; then
-	if [ ! -d /media/sdcard/backup/gambatte/ ]; then
+if [ -d $INTPATH/.gambatte/ ]; then
+	if [ ! -d $EXTPATH/backup/gambatte/ ]; then
 		echo "Gambatte backup folder doesn't exist, creating folder"
-		mkdir /media/sdcard/backup/gambatte
+		mkdir $EXTPATH/backup/gambatte
 	fi
 	echo "Backing up Gambatte data"
-	rsync --update -rt /media/data/local/home/.gambatte/ /media/sdcard/backup/gambatte
+	rsync --update -rt $INTPATH/.gambatte/ $EXTPATH/backup/gambatte
 fi
 
 # Backs up ReGBA data
-if [ -d /media/data/local/home/.gpsp/ ]; then
-	if [ ! -d /media/sdcard/backup/gpsp/ ]; then
+if [ -d $INTPATH/.gpsp/ ]; then
+	if [ ! -d $EXTPATH/backup/gpsp/ ]; then
 		echo "ReGBA backup folder doesn't exist, creating folder"
-		mkdir /media/sdcard/backup/gpsp
+		mkdir $EXTPATH/backup/gpsp
 	fi
 	echo "Backing up ReGBA data"
-	rsync --update -rt /media/data/local/home/.gpsp/ /media/sdcard/backup/gpsp
+	rsync --update -rt $INTPATH/.gpsp/ $EXTPATH/backup/gpsp
 fi
 
 # Backs up PCSX4all data
-if [ -d /media/data/local/home/.pcsx4all/ ]; then
-	if [ ! -d /media/sdcard/backup/pcsx4all/ ]; then
+if [ -d $INTPATH/.pcsx4all/ ]; then
+	if [ ! -d $EXTPATH/backup/pcsx4all/ ]; then
 		echo "PCSX4all backup folder doesn't exist, creating folder"
-		mkdir /media/sdcard/backup/pcsx4all
+		mkdir $EXTPATH/backup/pcsx4all
 	fi
 	echo "Backing up PCSX4all data"
-	rsync --update -rt /media/data/local/home/.pcsx4all/ /media/sdcard/backup/pcsx4all
+	rsync --update -rt $INTPATH/.pcsx4all/ $EXTPATH/backup/pcsx4all
 fi
 
 # Backs up Picodrive data
-if [ -d /media/data/local/home/.picodrive/ ]; then
-	if [ ! -d /media/sdcard/backup/picodrive/ ]; then
+if [ -d $INTPATH/.picodrive/ ]; then
+	if [ ! -d $EXTPATH/backup/picodrive/ ]; then
 		echo "Picodrive backup folder doesn't exist, creating folder"
-		mkdir /media/sdcard/backup/picodrive
+		mkdir $EXTPATH/backup/picodrive
 	fi
 	echo "Backing up PicoDrive data"
-	rsync --update -rt /media/data/local/home/.picodrive/ /media/sdcard/backup/picodrive
+	rsync --update -rt $INTPATH/.picodrive/ $EXTPATH/backup/picodrive
 fi
 
 # Backs up PocketSNES data
-if [ -d /media/data/local/home/.snes96_snapshots/ ]; then
-	if [ ! -d /media/sdcard/backup/snes96_snapshots/ ]; then
+if [ -d $INTPATH/.snes96_snapshots/ ]; then
+	if [ ! -d $EXTPATH/backup/snes96_snapshots/ ]; then
 		echo "SNES96 backup folder doesn't exist, creating folder"
-		mkdir /media/sdcard/backup/snes96_snapshots
+		mkdir $EXTPATH/backup/snes96_snapshots
 	fi
 	echo "Backing up SNES96 data"
-	rsync --update -rt /media/data/local/home/.snes96_snapshots/ /media/sdcard/backup/snes96_snapshots
+	rsync --update -rt $INTPATH/.snes96_snapshots/ $EXTPATH/backup/snes96_snapshots
 fi
 
-if [ -d /media/data/local/home/.pocketsnes/ ]; then
-	if [ ! -d /media/sdcard/backup/pocketsnes/ ]; then
+if [ -d $INTPATH/.pocketsnes/ ]; then
+	if [ ! -d $EXTPATH/backup/pocketsnes/ ]; then
 		echo "PocketSNES backup folder doesn't exist, creating folder"
-		mkdir /media/sdcard/backup/pocketsnes
+		mkdir $EXTPATH/backup/pocketsnes
 	fi
 	echo "Backing up PocketSNES data"
-	rsync --update -rt /media/data/local/home/.pocketsnes/ /media/sdcard/backup/pocketsnes
+	rsync --update -rt $INTPATH/.pocketsnes/ $EXTPATH/backup/pocketsnes
 fi
 
 dialog --clear --backtitle "SaveSync v1.0" --title "Backup Complete" --msgbox "Save backup complete. Press START to exit." 10 30
