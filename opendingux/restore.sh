50a7b9ea1d8e66303d1c8fb8f1981509d465e972
NekoMichi <31323046+NekoMichi@users.noreply.github.com>
2020-07-10 13:19:49 +0100

    Minor cleanup
    Added path shortcuts and minor changes to on-screen text for better consistency.

--- opendingux/restore.sh
+++ opendingux/restore.sh
@@ -3,9 +3,12 @@
 # desc=Restore save data from external SD card
 # author=NekoMichi
 
+INTPATH="/media/data/local/home/"
+EXTPATH="/media/sdcard/"
+
 echo "===Restore Saves==="
 # Checks to see if there is a card inserted in slot 2
-if [ ! -d /media/sdcard/ ]; then
+if [ ! -d $EXTPATH ]; then
 	echo "Could not detect secondary micro SD card."
 	echo "Please make sure you have a secondary SD card inserted."
 	echo ""
@@ -14,7 +17,7 @@ if [ ! -d /media/sdcard/ ]; then
 fi
 
 # Checks to see if backup folder exists on card 2
-if [ ! -d /media/sdcard/backup/ ]; then
+if [ ! -d $EXTPATH/backup/ ]; then
 	echo "Backup folder not found on card-2."
 	echo "Please make sure card-2 has backup data stored."
 	echo ""
@@ -23,72 +26,72 @@ if [ ! -d /media/sdcard/backup/ ]; then
 fi
 
 # Restores FCEUX data
-if [ -d /media/sdcard/backup/fceux/ ]; then
-	if [ ! -d /media/data/local/home/.fceux/ ]; then
-		echo "FCEUX folder doesn't exist in home directory, creating folder"
-		mkdir /media/data/local/home/.fceux
+if [ -d $EXTPATH/backup/fceux/ ]; then
+	if [ ! -d $INTPATH/.fceux/ ]; then
+		echo "FCEUX folder doesn't exist in home directory, creating folder."
+		mkdir $INTPATH/.fceux
 	fi
-	echo "Restoring FCEUX data"
-	rsync --update -rt /media/sdcard/backup/fceux/ /media/data/local/home/.fceux
+	echo "Restoring FCEUX data..."
+	rsync --update -rt $EXTPATH/backup/fceux/ $INTPATH/.fceux
 fi
 
 # Restores Gambatte data
-if [ -d /media/sdcard/backup/gambatte/ ]; then
-	if [ ! -d /media/data/local/home/.gambatte/ ]; then
-		echo "Gambatte folder doesn't exist in home directory, creating folder"
-		mkdir /media/data/local/home/.gambatte
+if [ -d $EXTPATH/backup/gambatte/ ]; then
+	if [ ! -d $INTPATH/.gambatte/ ]; then
+		echo "Gambatte folder doesn't exist in home directory, creating folder."
+		mkdir $INTPATH/.gambatte
 	fi
-	echo "Restoring Gambatte data"
-	rsync --update -rt /media/sdcard/backup/gambatte/ /media/data/local/home/.gambatte
+	echo "Restoring Gambatte data..."
+	rsync --update -rt $EXTPATH/backup/gambatte/ $INTPATH/.gambatte
 fi
 
 # Restores ReGBA data
-if [ -d /media/sdcard/backup/gpsp/ ]; then
-	if [ ! -d /media/data/local/home/.gpsp/ ]; then
-		echo "ReGBA folder doesn't exist in home directory, creating folder"
-		mkdir /media/data/local/home/.gpsp
+if [ -d $EXTPATH/backup/gpsp/ ]; then
+	if [ ! -d $INTPATH/.gpsp/ ]; then
+		echo "ReGBA folder doesn't exist in home directory, creating folder."
+		mkdir $INTPATH/.gpsp
 	fi
-	echo "Restoring ReGBA data"
-	rsync --update -rt /media/sdcard/backup/gpsp/ /media/data/local/home/.gpsp
+	echo "Restoring ReGBA data..."
+	rsync --update -rt $EXTPATH/backup/gpsp/ $INTPATH/.gpsp
 fi
 
 # Restores PCSX4all data
-if [ -d /media/sdcard/backup/pcsx4all/ ]; then
-	if [ ! -d /media/data/local/home/.pcsx4all/ ]; then
-		echo "PCSX4all folder doesn't exist in home directory, creating folder"
-		mkdir /media/data/local/home/.pcsx4all
+if [ -d $EXTPATH/backup/pcsx4all/ ]; then
+	if [ ! -d $INTPATH/.pcsx4all/ ]; then
+		echo "PCSX4all folder doesn't exist in home directory, creating folder."
+		mkdir $INTPATH/.pcsx4all
 	fi
-	echo "Restoring PCSX4all data"
-	rsync --update -rt /media/sdcard/backup/pcsx4all/ /media/data/local/home/.pcsx4all
+	echo "Restoring PCSX4all data..."
+	rsync --update -rt $EXTPATH/backup/pcsx4all/ $INTPATH/.pcsx4all
 fi
 
 # Restores Picodrive data
-if [ -d /media/sdcard/backup/picodrive/ ]; then
-	if [ ! -d /media/data/local/home/.picodrive/ ]; then
-		echo "Picodrive folder doesn't exist in home directory, creating folder"
-		mkdir /media/data/local/home/.picodrive
+if [ -d $EXTPATH/backup/picodrive/ ]; then
+	if [ ! -d $INTPATH/.picodrive/ ]; then
+		echo "Picodrive folder doesn't exist in home directory, creating folder."
+		mkdir $INTPATH/.picodrive
 	fi
-	echo "Restoring PicoDrive data"
-	rsync --update -rt /media/sdcard/backup/picodrive/ /media/data/local/home/.picodrive
+	echo "Restoring PicoDrive data..."
+	rsync --update -rt $EXTPATH/backup/picodrive/ $INTPATH/.picodrive
 fi
 
 # Backs up PocketSNES data
-if [ -d /media/sdcard/backup/snes96_snapshots/ ]; then
-	if [ ! -d /media/data/local/home/.snes96_snapshots/ ]; then
-		echo "SNES96 folder doesn't exist in home directory, creating folder"
-		mkdir /media/data/local/home/.snes96_snapshots
+if [ -d $EXTPATH/backup/snes96_snapshots/ ]; then
+	if [ ! -d $INTPATH/.snes96_snapshots/ ]; then
+		echo "SNES96 folder doesn't exist in home directory, creating folder."
+		mkdir $INTPATH/.snes96_snapshots
 	fi
-	echo "Restoring SNES96 data"
-	rsync --update -rt /media/sdcard/backup/snes96_snapshots/ /media/data/local/home/.snes96_snapshots
+	echo "Restoring SNES96 data..."
+	rsync --update -rt $EXTPATH/backup/snes96_snapshots/ $INTPATH/.snes96_snapshots
 fi
 
-if [ -d /media/sdcard/backup/pocketsnes/ ]; then
-	if [ ! -d /media/data/local/home/.pocketsnes/ ]; then
-		echo "PocketSNES folder doesn't exist in home directory, creating folder"
-		mkdir /media/data/local/home/.pocketsnes
+if [ -d $EXTPATH/backup/pocketsnes/ ]; then
+	if [ ! -d $INTPATH/.pocketsnes/ ]; then
+		echo "PocketSNES folder doesn't exist in home directory, creating folder."
+		mkdir $INTPATH/.pocketsnes
 	fi
-	echo "Backing up PocketSNES data"
-	rsync --update -rt /media/sdcard/backup/pocketsnes/ /media/data/local/home/.pocketsnes
+	echo "Backing up PocketSNES data..."
+	rsync --update -rt $EXTPATH/backup/pocketsnes/ $INTPATH/.pocketsnes
 fi
 
 dialog --clear --backtitle "SaveSync v1.0" --title "Restore Complete" --msgbox "Save restore complete. Press START to exit." 10 30
