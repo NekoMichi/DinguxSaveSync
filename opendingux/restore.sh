d27f45546a113db5b39892b6ef5bb0638b24114f
NekoMichi <31323046+NekoMichi@users.noreply.github.com>
2020-08-06 14:55:19 +0100

    Update paths

--- opendingux/restore.sh
+++ opendingux/restore.sh
@@ -4,7 +4,7 @@
 # author=NekoMichi
 
 # Checks to see if there is a card inserted in slot 2
-if [ ! -d /media/sdcard ]; then
+if [ ! -b /dev/mmcblk1 ]; then
 	dialog --clear --backtitle "SaveSync $APPVERSION" --title "No SD Card Found" --msgbox "No SD card inserted in slot-2.\n\nPress START to exit." 8 29
 	exit
 fi
