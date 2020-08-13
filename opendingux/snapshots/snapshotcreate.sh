6e2a4f10d5b282b6cd43ae33b4856abbdacb6ac3
NekoMichi <31323046+NekoMichi@users.noreply.github.com>
2020-08-13 12:06:24 +0100

    Cleaned up mkdir commands

--- opendingux/snapshots/snapshotcreate.sh
+++ opendingux/snapshots/snapshotcreate.sh
@@ -30,9 +30,7 @@ chmod -R 777 "$EXPORTPATH"
 
 # Backs up FCEUX data
 if [ -d $INTPATH/.fceux/ ]; then
-	mkdir "$EXPORTPATH/fceux"
-	mkdir "$EXPORTPATH/fceux/sav"
-	mkdir "$EXPORTPATH/fceux/fcs"
+	mkdir -p "$EXPORTPATH/fceux/sav" "$EXPORTPATH/fceux/fcs"
 	echo "Backing up FCEUX data..."
 	rsync -rtvhW $INTPATH/.fceux/sav/ "$EXPORTPATH/fceux/sav"
 	rsync -rtvhW $INTPATH/.fceux/fcs/ "$EXPORTPATH/fceux/fcs"
@@ -40,16 +38,14 @@ fi
 
 # Backs up Gambatte data
 if [ -d $INTPATH/.gambatte/ ]; then
-	mkdir "$EXPORTPATH/gambatte"
-	mkdir "$EXPORTPATH/gambatte/saves"
+	mkdir -p "$EXPORTPATH/gambatte/saves"
 	echo "Backing up Gambatte data..."
 	rsync -rtvhW $INTPATH/.gambatte/saves/ "$EXPORTPATH/gambatte/saves"
 fi
 
 # Backs up OhBoy data
 if [ -d $INTPATH/.ohboy/ ]; then
-	mkdir "$EXPORTPATH/ohboy"
-	mkdir "$EXPORTPATH/ohboy/saves"
+	mkdir -p "$EXPORTPATH/ohboy/saves"
 	echo "Backing up OhBoy data..."
 	rsync -rtvhW $INTPATH/.ohboy/saves/ "$EXPORTPATH/ohboy/saves"
 fi
@@ -63,9 +59,7 @@ fi
 
 # Backs up PCSX4all data
 if [ -d $INTPATH/.pcsx4all/ ]; then
-	mkdir "$EXPORTPATH/pcsx4all"
-	mkdir "$EXPORTPATH/pcsx4all/memcards"
-	mkdir "$EXPORTPATH/pcsx4all/sstates"
+	mkdir -p "$EXPORTPATH/pcsx4all/memcards" "$EXPORTPATH/pcsx4all/sstates"
 	echo "Backing up PCSX4all data..."
 	rsync -rtvhW $INTPATH/.pcsx4all/memcards/ "$EXPORTPATH/pcsx4all/memcards"
 	rsync -rtvhW $INTPATH/.pcsx4all/sstates/ "$EXPORTPATH/pcsx4all/sstates"
@@ -73,9 +67,7 @@ fi
 
 # Backs up Picodrive data
 if [ -d $INTPATH/.picodrive/ ]; then
-	mkdir "$EXPORTPATH/picodrive"
-	mkdir "$EXPORTPATH/picodrive/mds"
-	mkdir "$EXPORTPATH/picodrive/srm"
+	mkdir -p "$EXPORTPATH/picodrive/mds" "$EXPORTPATH/picodrive/srm"
 	echo "Backing up PicoDrive data..."
 	rsync -rtvhW $INTPATH/.picodrive/mds/ "$EXPORTPATH/picodrive/mds"
 	rsync -rtvhW $INTPATH/.picodrive/srm/ "$EXPORTPATH/picodrive/srm"
@@ -83,18 +75,14 @@ fi
 
 # Backs up SMS Plus data
 if [ -d $INTPATH/.smsplus/ ]; then
-	mkdir "$EXPORTPATH/smsplus"
-	mkdir "$EXPORTPATH/smsplus/sram"
-	mkdir "$EXPORTPATH/smsplus/state"
+	mkdir -p "$EXPORTPATH/smsplus/sram" "$EXPORTPATH/smsplus/state"
 	echo "Backing up SMS Plus data..."
 	rsync -rtvhW $INTPATH/.smsplus/sram/ "$EXPORTPATH/smsplus/sram"
 	rsync -rtvhW $INTPATH/.smsplus/state/ "$EXPORTPATH/smsplus/state"
 fi
 
 if [ -d $INTPATH/.sms_sdl/ ]; then
-	mkdir "$EXPORTPATH/sms_sdl"
-	mkdir "$EXPORTPATH/sms_sdl/sram"
-	mkdir "$EXPORTPATH/sms_sdl/state"
+	mkdir -p "$EXPORTPATH/sms_sdl/sram" "$EXPORTPATH/sms_sdl/state"
 	echo "Backing up SMS SDL data..."
 	rsync -rtvhW $INTPATH/.sms_sdl/sram/ "$EXPORTPATH/sms_sdl/sram"
 	rsync -rtvhW $INTPATH/.sms_sdl/state/ "$EXPORTPATH/sms_sdl/state"
@@ -115,9 +103,7 @@ fi
 
 # Backs up Snes9x data
 if [ -d $INTPATH/.snes9x/ ]; then
-	mkdir "$EXPORTPATH/snes9x"
-	mkdir "$EXPORTPATH/snes9x/spc"
-	mkdir "$EXPORTPATH/snes9x/sram"
+	mkdir -p "$EXPORTPATH/snes9x/spc" "$EXPORTPATH/snes9x/sram"
 	echo "Backing up Snes9x data..."
 	rsync -rtvhW $INTPATH/.snes9x/spc/ "$EXPORTPATH/snes9x/spc"
 	rsync -rtvhW $INTPATH/.snes9x/sram/ "$EXPORTPATH/snes9x/sram"
@@ -125,9 +111,7 @@ fi
 
 # Backs up SwanEmu data
 if [ -d $INTPATH/.swanemu/ ]; then
-	mkdir "$EXPORTPATH/swanemu"
-	mkdir "$EXPORTPATH/swanemu/eeprom"
-	mkdir "$EXPORTPATH/swanemu/sstates"
+	mkdir -p "$EXPORTPATH/swanemu/eeprom" "$EXPORTPATH/swanemu/sstates"
 	echo "Backing up SwanEmu data..."
 	rsync -rtvhW $INTPATH/.swanemu/eeprom/ "$EXPORTPATH/swanemu/eeprom"
 	rsync -rtvhW $INTPATH/.swanemu/sstates/ "$EXPORTPATH/swanemu/sstates"
@@ -135,9 +119,7 @@ fi
 
 # Backs up Temper data
 if [ -d $INTPATH/.temper/ ]; then
-	mkdir "$EXPORTPATH/temper"
-	mkdir "$EXPORTPATH/temper/bram"
-	mkdir "$EXPORTPATH/temper/save_states"
+	mkdir -p "$EXPORTPATH/temper/bram" "$EXPORTPATH/temper/save_states"
 	echo "Backing up Temper data..."
 	rsync -rtvhW $INTPATH/.temper/bram/ "$EXPORTPATH/temper/bram"
 	rsync -rtvhW $INTPATH/.temper/save_states/ "$EXPORTPATH/temper/save_states"
